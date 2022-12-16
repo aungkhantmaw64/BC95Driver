2d5efed6f8508d2ac0e2306d8de2fe06aa03e1a8
Aung Khant Maw <aungkhantmaw64@gmail.com>
2022-12-16 13:03:08 +0700

    Generate mocks for new SerialIO interface

--- mocks/fake_time_service.h
+++
@@ -1,9 +0,0 @@
-#ifndef D_FAKE_TIME_SERVICE_H
-#define D_FAKE_TIME_SERVICE_H
-
-#include "time_service.h"
-
-void FakeTimeService_Create(uint32_t initTime, uint8_t step);
-void FakeTimeService_Destroy(void);
-
-#endif
