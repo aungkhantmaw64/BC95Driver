af804446ab3fd35bbe75bf561559fafdc2883212
Aung Khant Maw <aungkhantmaw64@gmail.com>
2022-12-16 14:59:24 +0700

    Add test code for timeservice

---
+++ test/test_TimeService.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "TimeService.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_TimeService_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement TimeService");
+}
+
+#endif // TEST
