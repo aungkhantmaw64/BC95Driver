659098ebc3fe7016221cf1901f417614a696ccf2
Aung Khant Maw <aungkhantmaw64@gmail.com>
2022-12-16 13:05:04 +0700

    Add empty template test file

---
+++ test/test_SerialIO.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "SerialIO.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_SerialIO_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement SerialIO");
+}
+
+#endif // TEST
