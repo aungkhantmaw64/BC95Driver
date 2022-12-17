887eb4e1a3994fd9524338ff74c97e43354a4df4
Aung Khant Maw <aungkhantmaw64@gmail.com>
2022-12-17 17:51:08 +0700

    Update documentations

--- src/Modem.h
+++ src/Modem.h
@@ -3,8 +3,6 @@
 
 #include "SerialIO.h"
 
-typedef struct ModemStruct *ModemController;
-
 #define MAX_BUFFER_SIZE 50
 
 enum
@@ -12,24 +10,59 @@ enum
     CMD_TIMEOUT_ERROR = -2,
     CMD_FAILED = -1,
     CMD_SUCCESS = 0
-};
+}; /* Command response status */
 
 typedef enum UEFunctionEnum
 {
     UE_LEVEL_MINIMUM,
     UE_LEVEL_FULL,
-} UEFunction_t;
+} UEFunction_t; /* UE Functionality Level */
 
-struct ModemStruct
+typedef struct ModemControllerStruct *ModemController;
+
+struct ModemControllerStruct
 {
     SerialIO_t serial;
     int resetPin;
-    char rxBuffer[MAX_BUFFER_SIZE];
-};
+    char responseBuffer[MAX_BUFFER_SIZE];
+}; /* Struct containing modem infos*/
 
+/**
+ * @brief Create a modem controller instance
+ *
+ * @param serial Pointer to the UART peripheral the modem accociates with
+ * @param resetPin Digital IO pin to reboot the modem by hardware
+ * @return ModemController if success, NULL otherwise
+ */
 ModemController ModemController_Create(SerialIO_t serial, int resetPin);
+/**
+ * @brief Free the modem controller and its memory allocations
+ *
+ * @param modem The modem
+ */
 void ModemController_Destroy(ModemController modem);
+/**
+ * @brief Reboot the modem by software
+ *
+ * @param modem The modem
+ * @return Command reponse status
+ */
 int ModemController_RebootUE(ModemController modem);
+/**
+ * @brief Check if the connection to the modem is correct
+ *        and ready to receive commands
+ *
+ * @param modem The modem
+ * @return 1 if ready. 0 if not.
+ */
 int ModemController_IsReady(ModemController modem);
+/**
+ * @brief Set the UE phone function
+ *
+ * @param modem The modem
+ * @param mode Functionality level
+ * @return Command response status
+ */
 int ModemController_SetUEFunction(ModemController modem, UEFunction_t mode);
+
 #endif // MODEM_H
