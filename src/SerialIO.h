98cce3d795cac7f23e37563693769f97c5cb4def
Aung Khant Maw <aungkhantmaw64@gmail.com>
2022-12-17 17:29:00 +0700

    Fix a few typos in the comments

--- src/SerialIO.h
+++ src/SerialIO.h
@@ -23,44 +23,44 @@ SerialIO_t SerialIO_Create(int number, uint32_t baudrate);
 /**
  * @brief Free the UART device and its memory allocation
  *
- * @param serial pointer to the UART device struct
+ * @param serial The pointer to the UART device struct
  */
 void SerialIO_Destroy(SerialIO_t serial);
 /**
  * @brief Read a single byte from the rx buffer
  *
- * @param serial pointer to the UART device struct
+ * @param serial The pointer to the UART device struct
  * @return char if rx buffer is not empty
  */
 char SerialIO_Read(SerialIO_t serial);
 /**
  * @brief Write a single byte to the tx buffer
  *
- * @param serial pointer to the UART device struct
- * @param byte character byte to be written
+ * @param serial The pointer to the UART device struct
+ * @param byte Character byte to be written
  */
 void SerialIO_Write(SerialIO_t serial, char byte);
 /**
  * @brief Read bytes from the rxbuffer until the given character
  *        is seen
- * @param serial pointer to the UART device struct
- * @param buffer buffer where read bytes will be stored
- * @param end end character where the read process is stopped
- * @param timeout_ms timeout to wait for the given character
+ * @param serial The pointer to the UART device struct
+ * @param buffer Buffer where read bytes will be stored
+ * @param end End character where the read process is stopped
+ * @param timeout_ms Timeout to wait for the given character
  */
 void SerialIO_ReadStringUntil(SerialIO_t serial, char *buffer, char end, uint32_t timeout_ms);
 /**
  * @brief Write a string lateral to the tx buffer
  *
- * @param serial pointer to the UART device struct
- * @param text string lateral to be written
+ * @param serial The pointer to the UART device struct
+ * @param text String lateral to be written
  */
 void SerialIO_Print(SerialIO_t serial, const char *text);
 /**
  * @brief Fetch the number of bytes in the rx buffer
  *
- * @param serial pointer to the UART device struct
- * @return int number of bytes in the rx buffer
+ * @param serial The pointer to the UART device struct
+ * @return int Number of bytes in the rx buffer
  */
 int SerialIO_IsAvailable(SerialIO_t serial);
 
